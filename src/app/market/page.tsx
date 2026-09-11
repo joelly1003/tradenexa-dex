e7762ac9f5cf2638e63dd5e601f6838def9227fe
Drealguy <theojediji@gmail.com>
2026-09-12 00:14:20 +0100

    Update Market page to use the Pro DEX interface layout

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,10 +1,5 @@
-import { AdvancedTerminal } from '../../components/market/AdvancedTerminal';
+import { TradeInterface } from '../../components/trading/TradeInterface';
 
 export default function MarketPage() {
-  return (
-    <div className="-m-4 sm:-m-6 lg:-m-8">
-      {/* We use negative margins to make the terminal go full width of the main container */}
-      <AdvancedTerminal />
-    </div>
-  );
+  return <TradeInterface />;
 }
